8aab8609e294d8472264f2fb975df78b8276c4ec
LoazGM <lolzml454@gmail.com>
2026-01-13 20:58:57 +0700

    update finale day 2 (dengan opsional)

--- demo-unpam-day-2/dapps/contracts/contracts/simple-storage.sol
+++ demo-unpam-day-2/dapps/contracts/contracts/simple-storage.sol
@@ -4,12 +4,23 @@ pragma solidity ^0.8.20;
 contract SimpleStorage {
     //saya ingin menyimpan sebuah nilai dalam bentuk uint256
     uint256 private storedValue;
+    string public message;
+    address public owner;
 
     //ketika ada update, saya akan track perubahannya
     event ValueUpdated(uint256 newValue);
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
     //simpan nilai ke blockchain (write)
-    function setValue(uint256 _value) public {
+    function setValue(uint256 _value) public onlyOwner {
         storedValue = _value;
         emit ValueUpdated(_value);
     }
@@ -17,5 +28,10 @@ contract SimpleStorage {
     //membaca nilai dari blockchain (read) terakhir kali di update
     function getValue() public view returns (uint256) {
         return storedValue;
+    }
+
+    //menampilkan message
+    function setMessage(string memory _message) public onlyOwner {
+        message = _message;
     }
 }
